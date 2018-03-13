1425d5ee8bfc4bb31dc333545e4a9b3173a1954c
Kamran <kamran.raees96@gmail.com>
2018-03-13 15:32:20 +0500

    Assignment Complete

---
+++ app.js
@@ -0,0 +1,332 @@
+//1_ALERT TASKS
+
+// Task # 1
+// alert("Hello World");
+
+// Task # 2
+// alert("Error! Please Insert a valid password");
+
+// Task # 3
+// alert("Welcome to JS Land.. \n Happy Coding");
+
+// Task # 4
+// alert("Welcome to JS Land..");
+// alert("Happy Coding");
+
+// 2_Variable for Strings
+
+// Task # 1
+// var username;
+
+// Task # 2
+// var myName = "Muhamamd Kamran";
+
+// Task # 3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// Task # 4
+
+// var bioData = "Bio Data";
+// alert(bioData);
+// var name = "Muhammad Kamran";
+// alert(name);
+// var age = "21 yeras old";
+// alert(age);
+// var uniName = "SSUET";
+// alert(uniName);
+// var courseName = "Web & Mobile App Development from Saylani Mass Training";
+// alert(courseName);
+
+// Task # 5
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// // Task # 7
+// var email = "kamran.raees96@gmail.com";
+// alert(email);
+
+// Task # 8
+// var book = "'A smarter way to learn JavaScript'";
+// alert("I am trying to learn from the book, " + book);
+
+// Task # 9
+
+// var bioData = "Bio Data";
+// var name = "\nMy Name is Muhammad Kamran";
+// var age = "\nI am 21 yeras old";
+// var uniName = "\nI Study in SSUET";
+// var courseName = "\nI take Web & Mobile App Development from Saylani Mass Training";
+// alert(bioData + name + age + uniName + courseName);
+
+
+// Task # 10
+
+// document.write("Yay! I can write HTML Content through JaveScript");
+
+// 3_Variables for Numbers
+
+// Task # 1
+
+// var age = 21;
+// alert("I am " + age + " yeras old");
+
+
+// Task # 2 
+
+// var visited = 57;
+// alert("You have " + visited + " times.");
+
+// Task # 3
+
+// var birthYear = 1996;
+// document.write("My birth Year is: " + birthYear);
+
+// Task # 4
+// var name = "Kamran";
+// var shirts = 3;
+// var brandName = "Gul Ahmed";
+
+// document.write(name + " ordered " + shirts + " Kurtas " + " On " + brandName + " Collection.")
+
+
+
+// 5-7_Math Expressions
+
+// Task # 1
+
+// var a = +prompt("Enter 1st Number","0");
+// var b = +prompt("Enter 2nd  Number","0");;
+// var c = a+b;
+// alert("Sume of " + a + " & " + b + " is: " + c);
+
+// Task # 2
+
+// var a = +prompt("Enter 1st Number","0");
+// var b = +prompt("Enter 2nd  Number","0");
+// var c = a-b;
+// alert("Subtraction of " + a + " & " + b + " is: " + c);
+// var d = a*b;
+// alert("Multiplication of " + a + " & " + b + " is: " + d);
+// var e = a/b;
+// alert("Division of " + a + " & " + b + " is: " + e);
+// var f = a%b;
+// alert("Modulus of " + a + " & " + b + " is: " + f);
+
+// Task # 3
+
+// var value;
+// document.write("Vale after variable declearation is undefine<br>");
+// value = 3;
+// value = value++;
+// document.write("<br>Value after Increment is " + value);
+// value = value + 7;
+// document.write("<br>value afer addition is " + value);
+// value = value--;
+// document.write("<br>Value after Decrement is " + value);
+// value = value%3;
+// document.write("<br>The Reminder is: " + value);
+
+// Task # 4
+
+// var cost = 600;
+// var ticket = 5;
+// var totalCost = cost*ticket;
+// alert("Total Cost to buy 5 ticket to a movie is: Rs. " + totalCost);
+
+// Task # 5
+// var num = prompt("Enter Number", "0") //prompt user to enter the number
+ // var num = parseInt(num); //parse the num to number
+// var i = 0;
+ // document.write('<table border="1" cellspacing="0">');
+// for(i=1;i<10;i++) {
+//     document.write("<tr><td>" + num + " x " + i + " = " + num*i + "</td></tr>");
+// }
+// document.write("</table>");
+
+// Task # 9
+
+// var usDollar = 10;
+// var usDollartoPakistanRupee = 104;
+// var saudiRiyal = 25;
+// var saudiRiyaltoPakistanRupee = 28;
+// var totalCurrency = (usDollar*usDollartoPakistanRupee) + (saudiRiyal*saudiRiyaltoPakistanRupee);
+// alert("The Total Currency in PKR: " + totalCurrency);
+
+
+// Task # 10
+// var a = +prompt("Enter Number", "0");
+
+// var b = (((a+5)*10)/2);
+
+// alert("Result: " + b);
+
+// Task # 11
+
+// var currentYear = +Promt("Enter Current Year", "2017");
+// var birthYear = +Promt("Enter Birth  Year", "0000");;
+// var exceptedMinimumAge = currentYear - birthYear;
+// var exceptedMaximumAge = exceptedMinimumAge++;
+// document.write("The Age Calculator<br> Current Year: " + currentYear + "<br>Birth Year: " + birthYear + "<br><br>They are either" + exceptedMinimumAge + " or " + exceptedMaximumAge + " years old");
+
+// Task # 12
+
+// var a = 10;
+// document.write("The Value of a is:" + a + "<br>");
+
+// ++a;
+// document.write("The Value of ++a is:" + a + "<br>");
+// document.write("Now the Value of a is:" + a + "<br>");
+
+// a++;
+// document.write("The Value of a++ is:" + a + "<br>");
+// document.write("Now the Value of a is:" + a + "<br>");
+
+// --a;
+// document.write("The Value of --a is:" + a + "<br>");
+// document.write("Now the Value of a is:" + a + "<br>");
+
+// a--;
+// document.write("The Value of a-- is:" + a + "<br>");
+// document.write("Now the Value of a is:" + a + "<br>");
+
+
+// Task # 13
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write(result);
+
+// 8-11_ConcatenatingString_Prompts
+
+// Task # 1
+
+// var name = prompt("Enter Your name");
+// alert("Hi, " + name);
+
+// Task # 2
+// var num = prompt("Enter Number", "5") //prompt user to enter the number
+//  var num = parseInt(num); //parse the num to number
+// var i = 0;
+//  document.write('<table border="1" cellspacing="0">');
+// for(i=1;i<10;i++) {
+//     document.write("<tr><td>" + num + " x " + i + " = " + num*i + "</td></tr>");
+// }
+// document.write("</table>");
+
+// Task # 3
+
+// var cityName = prompt("Enter your city name");
+// alert("Welcome to city of lights");
+
+// Task # 4
+
+
+// var gender = prompt("Enter your gender");
+// if(gender === "Male"){
+//     alert("Good Morning Sir!");
+// }
+
+// if(gender === "Female"){
+//     alert("Good Morning Maam!");
+// }
+
+
+// Task # 5
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+
+// Task # 6
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+
+// Task # 7
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// Task # 8
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+// Task # 9
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+
+// var str = prompt("Enter some text");
+// var numChars = str.length;
+// for (var i = 0; i < numChars; i++) {
+// if (str.slice(i, i + 2) === "  ") {
+// alert("No double spaces!");
+// break;
+// }
+// }
+
+
+
+// var text = "The New Yorker magazine doesn't allow the phrase World War II. So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers.";
+// for (var i = 0; i < text.length; i++) {
+//     if (text.slice(i, i + 12) === "World War II") {
+//     text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//     alert(text);
+//     }
+//     }
+
+
+// var firstChar = text.indexOf("World War II");
+// if (firstChar !== -1) {
+// text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+// alert(text);
+// }
+// alert(text);
+
+// for (var i = 0; i < text.length; i++) {
+// if (text.charAt(i) === "!") {
+// alert("Exclamation point found!");
+// break;
+// }
+// }
+
+// function test(number)
+// {
+//    while(number < 5) {
+//       number++;
+//    }
+//    return number;
+// }
+// alert(test(2));
+
+function multNmbrs(a, b) {
+    var c = a*b;
+}
+multNmbrs(2, 6);
